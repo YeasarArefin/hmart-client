3f5c4c66cd488ca1ac7c4eb8a6d7a1f1d3f7ef39
YeasarArefin <yeasararefin007@gmail.com>
2021-11-12 01:41:15 +0600

    header button added

--- src/pages/Home/Header.js
+++ src/pages/Home/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import laptop from '../../assets/laptop-bg.png';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
@@ -12,7 +13,9 @@ const Header = () => {
                     <div className="flex flex-col gap-y-10">
 
                         <h1 className="text-white text-7xl font-bold">BesT Deals Here To Buy Laptops</h1>
-                        <button className="border-2 border-white rounded-lg bg-transparent text-white w-44 py-3 hover:bg-white hover:text-gray-600">Shop All Device</button>
+                        <Link to="/explore">
+                            <button className="border-2 border-white rounded-lg bg-transparent text-white w-44 py-3 hover:bg-white hover:text-gray-600">Shop All Device</button>
+                        </Link>
 
                     </div>
 
